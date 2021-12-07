cfd47d271ad186c6aa9ea99812c52d9b8e026854
yashagozwan <yashagozwan13@gmail.com>
2021-12-08 03:31:04 +0700

    database url

--- src/database/promisePool.js
+++ src/database/promisePool.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: 'b6qhwtcq2mct5p4y8khb-mysql.services.clever-cloud.com',
   port: '3306',
-  user: 'root',
-  password: 'root',
-  database: 'playground',
+  user: 'uf0uive1x7qiw1qu',
+  password: 'DAWRYuhqwpMkbFzPr36p',
+  database: 'b6qhwtcq2mct5p4y8khb',
 });
 
 const promisePool = pool.promise();
